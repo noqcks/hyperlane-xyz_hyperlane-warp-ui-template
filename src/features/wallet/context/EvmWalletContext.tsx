1a413cf4f1b81328a733eb60ca3e22992d46928d
noqcks <benji@093b.org>
2024-04-04 14:38:00 +0000

    Upgrade dependencies

--- src/features/wallet/context/EvmWalletContext.tsx
+++ src/features/wallet/context/EvmWalletContext.tsx
@@ -12,7 +12,10 @@ import {
   walletConnectWallet,
 } from '@rainbow-me/rainbowkit/wallets';
 import { PropsWithChildren, useMemo } from 'react';
-import { WagmiConfig, configureChains, createConfig } from 'wagmi';
+import { createConfig } from 'wagmi';
+import { coinbaseWallet, injected, walletConnect } from 'wagmi/connectors';
+import { http } from '@wagmi/core';
+import { mainnet, sepolia } from 'wagmi/chains';
 import { publicProvider } from 'wagmi/providers/public';
 
 import { ProtocolType } from '@hyperlane-xyz/utils';
@@ -56,11 +59,16 @@ const connectors = connectorsForWallets([
 ]);
 
 const wagmiConfig = createConfig({
+  chains: [mainnet, sepolia],
   connectors: [
     injected(),
     coinbaseWallet({ appName: 'YourAppName' }),
     walletConnect({ projectId: process.env.YOUR_PROJECT_ID }),
   ],
+  transports: {
+    [mainnet.id]: http(),
+    [sepolia.id]: http(),
+  },
 });
 
 export function EvmWalletContext({ children }: PropsWithChildren<unknown>) {
