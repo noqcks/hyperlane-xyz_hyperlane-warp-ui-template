caab5f9a1c611da42924c5adcd828c8e93d5936e
noqcks <benji@093b.org>
2024-04-04 14:19:58 +0000

    Upgrade dependencies

--- src/features/wallet/context/EvmWalletContext.tsx
+++ src/features/wallet/context/EvmWalletContext.tsx
@@ -37,23 +37,23 @@ const connectors = connectorsForWallets([
   {
     groupName: 'Recommended',
     wallets: [
-      metaMaskWallet(connectorConfig),
-      injectedWallet(connectorConfig),
-      walletConnectWallet(connectorConfig),
-      ledgerWallet(connectorConfig),
+      metaMaskWallet({chains}),
+      injectedWallet({chains}),
+      walletConnectWallet({chains}),
+      ledgerWallet({chains}),
     ],
   },
   {
     groupName: 'More',
     wallets: [
-      coinbaseWallet(connectorConfig),
-      omniWallet(connectorConfig),
-      rainbowWallet(connectorConfig),
-      trustWallet(connectorConfig),
-      argentWallet(connectorConfig),
+      coinbaseWallet({appName: 'RainbowKit App', chains}),
+      omniWallet({chains}),
+      rainbowWallet({chains}),
+      trustWallet({chains}),
+      argentWallet({chains}),
     ],
   },
-]);
+], {appName: 'RainbowKit App', projectId: 'YOUR_PROJECT_ID'});
 
 const wagmiConfig = createConfig({
   autoConnect: true,
